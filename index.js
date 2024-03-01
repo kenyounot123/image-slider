ea69270fc21e1229ad7b8a3962251623bcdfa065
kenyounot123 <kenlu519@gmail.com>
2024-02-29 23:50:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-function next() {}
-function previous() {}
+const rightArrow = document.querySelector(".right-arrow");
+const leftArrow = document.querySelector(".left-arrow");
+const slides = document.querySelectorAll(".slides");
+const carouselNavButtons = document.querySelectorAll(".dots");
+
+rightArrow.addEventListener("click", nextSlide);
+leftArrow.addEventListener("click", previousSlide);
+
+function nextSlide() {
+  // Hide current slide
+  // Show next slide
+}
+function previousSlide() {
+  // Hide current slide
+  // Show previous slide
+}
