1fe84e2242d258fb673f87b27a0fee5100c9839f
kenyounot123 <kenlu519@gmail.com>
2024-03-01 13:43:07 -0500

    Refactor showSlide function and add animations

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const rightArrow = document.querySelector(".right-arrow");
 const leftArrow = document.querySelector(".left-arrow");
-const slides = document.querySelectorAll(".slides");
+const slides = document.querySelectorAll(".slide");
 const carouselNavButtons = document.querySelectorAll(".dots");
+let index = 0;
+
+function showSlide(n) {
+  slides.forEach((slide) => (slide.style.display = "none"));
+
+  index = (n + slides.length) % slides.length;
+
+  slides[index].style.display = "block";
+  slides[index].classList.add("slide-transition");
+}
+
+function nextSlide() {
+  showSlide(index + 1);
+}
+
+function previousSlide() {
+  showSlide(index - 1);
+}
 
 rightArrow.addEventListener("click", nextSlide);
 leftArrow.addEventListener("click", previousSlide);
-
-function nextSlide() {
-  // Hide current slide
-  // Show next slide
-}
-function previousSlide() {
-  // Hide current slide
-  // Show previous slide
-}
